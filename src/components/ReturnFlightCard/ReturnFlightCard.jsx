0bf60907aacb1c12aa008a9250214f995f451cd8
Alasdair Blackwell <aliblackwell@protonmail.com>
2022-02-24 15:59:09 +0000

    Complete flight card layout

--- src/components/ReturnFlightCard/ReturnFlightCard.jsx
+++ src/components/ReturnFlightCard/ReturnFlightCard.jsx
@@ -3,14 +3,32 @@ import PropTypes from 'prop-types';
 import BpkCard from 'bpk-component-card';
 import STYLES from './ReturnFlightCard.scss';
 import FlightCardDetail from '../FlightCardDetail';
+import BpkText from 'bpk-component-text/src/BpkText';
 const getClassName = (className) => STYLES[className] || 'UNKNOWN';
 
-const ReturnFlightCard = ({ outboundFlight, inboundFlight, price, url }) => {
+import BpkButton from 'bpk-component-button';
+
+import { withDefaultProps } from 'bpk-react-utils';
+const LargeParagraph = withDefaultProps(BpkText, { textStyle: 'xl', tagName: 'p' });
+const MutedUrl = withDefaultProps(BpkText, {
+  textStyle: 'l',
+  tagName: 'p',
+  style: { color: 'rgb(178, 178, 191)' },
+});
+
+const ReturnFlightCard = ({ outboundFlight, inboundFlight, price, agent }) => {
   return (
     <section>
       <BpkCard className={getClassName('ReturnFlightCard')}>
         <FlightCardDetail flight={outboundFlight} />
         <FlightCardDetail flight={inboundFlight} />
+        <div className={getClassName('ReturnFlightCard__layout-container')}>
+          <div>
+            <LargeParagraph>{price}</LargeParagraph>
+            <MutedUrl>{agent}</MutedUrl>
+          </div>
+          <BpkButton large={false} className={getClassName('ReturnFlightCard__button')}>Select</BpkButton>
+        </div>
       </BpkCard>
     </section>
   );
@@ -20,7 +38,7 @@ ReturnFlightCard.propTypes = {
   outboundFlight: PropTypes.object,
   inboundFlight: PropTypes.object,
   price: PropTypes.string,
-  url: PropTypes.string,
+  agent: PropTypes.string,
 };
 
 export default ReturnFlightCard;
